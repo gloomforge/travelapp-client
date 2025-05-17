fcb4f57646699106e42513e19e88c0354d81c3ee
gloomforge <softly2200@gmail.com>
2025-05-17 11:02:36 +0300

    feat(not-found): add static layout for notFound page

---
+++ src/pages/notFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import './NotFound.css';
+
+function NotFound() {
+    return (
+        <div className={'not-found'}>
+            <img src={'/images/404NotFound.png'} alt={'not found'} />
+        </div>
+    );
+}
+
+export default NotFound;
