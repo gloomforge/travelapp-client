c6247504e1dbf9d396c79c088c068141b1c6d04a
Gloomforge <softly2200@gmail.com>
2025-05-25 19:47:33 +0300

    refactor(api): update authentication and user API methods

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -1,20 +1,24 @@
 import AuthResponse from "../models/output/AuthResponse";
 import RegisterRequest from "../models/input/RegisterRequest";
 import LoginRequest from "../models/input/LoginRequest";
-import axios from "axios";
+import axiosInstance from "../config/AxiosConfig";
 import {API_URL} from "../config/BaseUrl";
 
 class AuthApi {
-  async login(data: LoginRequest): Promise<AuthResponse> {
-    const { data: result } = await axios.post<AuthResponse>(`${API_URL}/auth/login`, data);
-    return result;
-  }
+    async login(data: LoginRequest): Promise<AuthResponse> {
+        const { data: result } = await axiosInstance.post<AuthResponse>(`${API_URL}/auth/login`, data);
+        return result;
+    }
 
-  async register(data: RegisterRequest): Promise<AuthResponse> {
-    const { data: result } = await axios.post<AuthResponse>(`${API_URL}/auth/register`, data);
-    return result;
-  }
+    async register(data: RegisterRequest): Promise<AuthResponse> {
+        const { data: result } = await axiosInstance.post<AuthResponse>(`${API_URL}/auth/register`, data);
+        return result;
+    }
+
+    static async refreshToken(refreshToken: string): Promise<AuthResponse> {
+        const { data: result } = await axiosInstance.post<AuthResponse>(`/auth/refresh`, { refreshToken });
+        return result;
+    }
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default new AuthApi();
